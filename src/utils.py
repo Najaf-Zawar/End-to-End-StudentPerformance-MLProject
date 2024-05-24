c5cb55f68b7d85a658d807bbc925c8b811cd55f1
Najaf Zawar <zawarnajaf5@gmail.com>
2024-05-24 19:02:49 +0500

    Added Hyperparameter-Tunning

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import dill
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -23,13 +24,21 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         
         report = {}
-
+        logging.info("In Evaluate method")
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            # gs = GridSearchCV(model, parm, cv=3, n_jobs=3, verbose=0, refit=False)]
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -39,6 +48,7 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]]= test_model_score
+            logging.info(f"Iteration: {i}")
 
         return report
 
