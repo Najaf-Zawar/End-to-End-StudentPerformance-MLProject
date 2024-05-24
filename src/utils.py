58f7893aad4d2043988757c5fdfa9ca62fe0d94f
Najaf Zawar <zawarnajaf5@gmail.com>
2024-05-24 19:56:29 +0500

    Added prediction_pipeline & flask_app

--- src/utils.py
+++ src/utils.py
@@ -48,9 +48,16 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]]= test_model_score
-            logging.info(f"Iteration: {i}")
 
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
